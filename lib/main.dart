66e14e81a2117d899abbd7cbeec36690d2b69be3
Shubham Bundela <sbundela88@gmail.com>
2020-09-28 23:47:54 +0530

    Adding Main File

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-
-import 'screens/intro_slider.dart';
+import 'package:payso/screens/intro_slider.dart';
+import 'package:payso/screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PaySo App',
-      home: IntroSlider(),
+      initialRoute: SplashScreen.id,
+      routes: {
+        SplashScreen.id: (context) => SplashScreen(),
+        IntroSlider.id: (context) => IntroSlider(),
+      },
     );
   }
 }
