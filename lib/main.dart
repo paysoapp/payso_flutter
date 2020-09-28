4e70168838f127238476c33560f8a0493843bf32
amitgeed <amitkumargeed@gmail.com>
2020-09-28 23:34:01 +0530

    added intial route and routes to the main.dart you can change intro_slider route as per your class name and route id

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:payso/screens/intro_slider.dart';
+import 'package:payso/screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Payso App',
+      title: 'PaySo App',
+      initialRoute: SplashScreen.id,
+      routes: {
+        SplashScreen.id: (context) => SplashScreen(),
+        IntroSlider.id: (context) => IntroSlider(),
+      },
     );
   }
 }
