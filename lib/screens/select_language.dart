132c805fd0c87081aa2726c73eaffa3a61a559bf
amitgeed <amitkumargeed@gmail.com>
2020-10-05 22:15:43 +0530

    created UI to select language

--- lib/screens/select_language.dart
+++ lib/screens/select_language.dart
@@ -1,76 +1,122 @@
-
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:payso/model/language.dart';
-import 'package:payso/main.dart';
 import 'package:payso/constants.dart';
 import 'intro_slider.dart';
-
 
 class SelectLanguage extends StatefulWidget {
   @override
   _SelectLanguageState createState() => _SelectLanguageState();
   static const String id = 'select_language';
-
-
 }
 
 class _SelectLanguageState extends State<SelectLanguage> {
-
-
-  // void _changeLanguage(Language language){
-  //   Locale _temp;
-  //   switch (language.languageCode){
-  //     case 'en':
-  //       _temp=Locale(language.languageCode,'en');
-  //       break;
-  //     case 'fil':
-  //       _temp=Locale(language.languageCode,'fil');
-  //       break;
-  //
-  //     default:
-  //       _temp=Locale(language.languageCode,'fil');
-  //   }
-  //   MyApp.setLocale(context,_temp);
-  // }
+  String selectedLang = Language.languageList().first.name;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Payso"),
-        centerTitle: true,
-        backgroundColor: cPrimaryColor,
-      ),
-
-
       body: Container(
-
         padding: EdgeInsets.all(20.0),
-        child: DropdownButton(onChanged:(Language language){
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Image.asset(
+                  'assets/images/language.png',
+                  height: MediaQuery.of(context).size.height * 0.3,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  'Choose your preferred language',
+                  textAlign: TextAlign.center,
+                  style: cTextStyle,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 1.1,
+                  decoration: BoxDecoration(
+                    color: cPrimaryColor,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(14),
+                    ),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  child: DropdownButton(
+                      isExpanded: true,
+                      iconSize: 30,
+                      iconEnabledColor: Colors.white,
+                      underline: SizedBox(),
+                      hint: Text(
+                        selectedLang,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onChanged: (Language language) {
+                        setState(() {
+                          selectedLang = language.name;
+                        });
 
-          if(EasyLocalization.of(context).locale.languageCode!=language.languageCode){
-
-            context.locale=Locale(language.languageCode,language.countryCode);
-
-          }
-
-      // _changeLanguage(language);
-      // print("Language Name "+language.languageCode);
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => IntroSlider()),
-      );
-
-      },
-            items: Language.languageList()
-                .map<DropdownMenuItem<Language>>((lang) => DropdownMenuItem(
-          value: lang,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children:<Widget> [Text(lang.flag),Text(lang.name)],
+                        if (EasyLocalization.of(context).locale.languageCode !=
+                            language.languageCode) {
+                          context.locale = Locale(
+                              language.languageCode, language.countryCode);
+                        }
+                      },
+                      items: Language.languageList()
+                          .map<DropdownMenuItem<Language>>(
+                              (lang) => DropdownMenuItem(
+                                    value: lang,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: <Widget>[
+                                        Text(lang.flag),
+                                        Text(lang.name)
+                                      ],
+                                    ),
+                                  ))
+                          .toList()),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => IntroSlider()),
+                    );
+                  },
+                  child: Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    decoration: BoxDecoration(
+                      color: cPrimaryColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(14),
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Save",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        )).toList()),
+        ),
       ),
     );
   }
